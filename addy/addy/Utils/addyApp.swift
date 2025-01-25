1f80826beadb095b3fb97e9072fe2ad5e10be1d8
Alok Sahay <alok.sahay87@gmail.com>
2025-01-25 11:02:08 +0100

    New xcode project

---
+++ addy/addy/Utils/addyApp.swift
@@ -0,0 +1,17 @@
+//
+//  addyApp.swift
+//  addy
+//
+//  Created by Alok Sahay on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct addyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(vm: ViewModel())
+        }
+    }
+}
---
+++ addy/addy/Utils/env.swift
@@ -0,0 +1,13 @@
+//
+//  Env.swift
+//  addy
+//
+//  Created by Alok Sahay on 24.01.2025.
+//
+
+import Foundation
+
+struct environment {
+    static let web3AuthClientId: String = "BPdWoOWu8Nk743CSr1vaqcY0edE0Rh2VJtDX9b8CT_0FQZyNpMILG2ZUP1cTo4Vw9By51iTdzOUV945Mb6q6fdg"
+    static let web3AuthRedirectURL = "com.alok.addy://auth"
+}
